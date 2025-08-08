14bd912b76e1d035a59d0614e7aae89062a9f5b1
Luis Rios Alvarado <luismanuelrios123@gmail.com>
2025-08-07 20:28:34 -0400

    Finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById("create-task-form");
+
+  function buildToDo(formInput) {
+    const taskList = document.getElementById("tasks");
+    const task = document.createElement("li");
+    task.textContent = formInput;
+    taskList.append(task);    
+  }
+  
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const formInput = document.getElementById("new-task-description").value;
+    buildToDo(formInput);
+    
+  });
 });
